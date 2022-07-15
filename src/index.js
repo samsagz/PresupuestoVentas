8fdf706c9fd0f1e69a5e71e8a53a7547139adc12
German Bedoya <monobedoya@gmail.com>
2022-07-15 11:11:33 -0500

    Updated Project to include Bootstrap
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './tab.css';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import './tab.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,32 +11,83 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.Fragment>
+        <ul class="nav nav-tabs" id="initialTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active"
+                    id="main" data-bs-toggle="tab" data-bs-target="#main-tab-pane"
+                    type="button" role="tab" aria-controls="main-tab-pane" aria-selected="true">Inicio</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="want" data-bs-toggle="tab" data-bs-target="#want-tab-pane"
+                    type="button" role="tab" aria-controls="want-tab-pane" aria-selected="false">1. Quiero</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="guess" data-bs-toggle="tab" data-bs-target="#guess-tab-pane"
+                    type="button" role="tab" aria-controls="guess-tab-pane" aria-selected="false">2. Creo</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="scenario" data-bs-toggle="tab" data-bs-target="#scenario-tab-pane"
+                    type="button" role="tab" aria-controls="scenario-tab-pane" aria-selected="false">3. Escenarios</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="client" data-bs-toggle="tab" data-bs-target="#client-tab-pane"
+                    type="button" role="tab" aria-controls="client-tab-pane" aria-selected="false">4. Cliente</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="breakeven" data-bs-toggle="tab" data-bs-target="#breakeven-tab-pane"
+                    type="button" role="tab" aria-controls="breakeven-tab-pane" aria-selected="false">5. Pto. Equilibrio</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="cac" data-bs-toggle="tab" data-bs-target="#cac-tab-pane"
+                    type="button" role="tab" aria-controls="cac-tab-pane" aria-selected="false">6. CAC</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="colection" data-bs-toggle="tab" data-bs-target="#colection-tab-pane"
+                    type="button" role="tab" aria-controls="colection-tab-pane" aria-selected="false">7. Coleccion</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link"
+                    id="budget" data-bs-toggle="tab" data-bs-target="#budget-tab-pane"
+                    type="button" role="tab" aria-controls="budget-tab-pane" aria-selected="false">PRESUPUESTO</button>
+            </li>
 
-        <input type="radio" name="tabs" id="tab1" checked />
-        <label for="tab1">Inicio</label>
-        <input type="radio" name="tabs" id="tab2" />
-        <label for="tab2">1. Quiero</label>
-        <input type="radio" name="tabs" id="tab3" />
-        <label for="tab3">2. Creo</label>
-        <input type="radio" name="tabs" id="tab4" />
-        <label for="tab4">3. Escenarios</label>
-        <input type="radio" name="tabs" id="tab5" />
-        <label for="tab5">4. Cliente</label>
-        <input type="radio" name="tabs" id="tab6" />
-        <label for="tab6">5. Pto. Equilibrio</label>
-        <input type="radio" name="tabs" id="tab7" />
-        <label for="tab7">6. CAC</label>
-        <input type="radio" name="tabs" id="tab8" />
-        <label for="tab8">7. Coleccion</label>
-        <input type="radio" name="tabs" id="tab9" />
-        <label for="tab9">PRESUPUESTO</label>
+        </ul>
 
-        <div class="tab content1">
-            <App />
+        <div class="tab-content" id="myMainTabContent">
+            <div class="tab-pane fade show active" id="main-tab-pane" role="tabpanel" aria-labelledby="main-tab" tabindex="0">
+                <App />
+            </div>
+            <div class="tab-pane fade" id="want-tab-pane" role="tabpanel" aria-labelledby="want-tab" tabindex="0">
+                ...
+            </div>
+            <div class="tab-pane fade" id="guess-tab-pane" role="tabpanel" aria-labelledby="guess-tab" tabindex="0">
+                ...
+            </div>
+            <div class="tab-pane fade" id="scenario-tab-pane" role="tabpanel" aria-labelledby="scenario-tab" tabindex="0">
+                ...
+            </div>
+            <div class="tab-pane fade" id="client-tab-pane" role="tabpanel" aria-labelledby="client-tab" tabindex="0">
+                ...
+            </div>
+            <div class="tab-pane fade" id="breakeven-tab-pane" role="tabpanel" aria-labelledby="breakeven-tab" tabindex="0">
+                ...
+            </div>
+            <div class="tab-pane fade" id="cac-tab-pane" role="tabpanel" aria-labelledby="cac-tab" tabindex="0">
+                costo de adquisición del cliente (Customer Acquisition Cost )
+            </div>
+            <div class="tab-pane fade" id="colection-tab-pane" role="tabpanel" aria-labelledby="colection-tab" tabindex="0">
+                ...
+            </div>
+            <div class="tab-pane fade" id="budget-tab-pane" role="tabpanel" aria-labelledby="budget-tab" tabindex="0">
+                ...
+            </div>
         </div>
-        <div class="tab content2">Tab2 Contents</div>
-        <div class="tab content3">Tab3 Contents</div>
-        <div class="tab content4">Tab4 Contents</div>
-        <div class="tab content5">Tab5 Contents</div>
     </React.Fragment>
 );
